8743ea5adc7820a19b8232cab48fc3152a3c64a5
uditha1994 <uditha@programmer.net>
2025-05-20 10:00:16 +0530

    create new story

--- js/app.js
+++ js/app.js
@@ -12,4 +12,47 @@ document.addEventListener('DOMContentLoaded', function () {
     const createStoryBtn = document.getElementById('create-story');
     const newStroryTitleInput = document.getElementById('new-story-title');
     const newStoryFirstParagraphInput = document.getElementById('new-story-first-paragraph');
+
+    //state
+    let currentStoryId = null;
+
+    function loadStories() {
+        // load all stories from firebase
+    }
+
+    function viewStory(id, story) {
+        //View a single Story
+    }
+
+    function createNewStory(title, firstParagraph) {
+        //Create a new story
+        if (!title || !firstParagraph) {
+            alert('Please fill in all fields');
+            return;
+        }
+
+        if(firstParagraph.length < 50){
+            alert('First paragraph must be at least 50 characters long');
+            return;
+        }
+
+        const newStoryref = database.ref('stories').push();
+        newStoryref.set({
+            title: title,
+            createAt: firebaseConfig.database.ServerValue.TIMESTAMP,
+            paragraphs: {
+                first:{
+                    text: firstParagraph,
+                    timestamp: firebase.database.ServerValue.TIMESTAMP,
+                    like:0
+                }
+            }
+        });
+    }
+
+    //Event Listeners
+    createStoryBtn.addEventListener('click', ()=>{
+        createNewStory(newStroryTitleInput.value, newStoryFirstParagraphInput.value);
+    });
+
 });
